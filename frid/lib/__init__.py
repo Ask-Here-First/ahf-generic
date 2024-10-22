4c147dc6ca812b9e87d1dccb086d383074a203c1
Hanhua Feng <hanhua.feng@askherefirst.com>
2024-10-22 14:28:34 -0400

    frid.lib: adding a list find helper function.

--- frid/lib/__init__.py
+++ frid/lib/__init__.py
@@ -1,4 +1,5 @@
 from .blobs import base64url_encode, base64url_decode
+from .lists import list_find_ex
 from .dicts import TransKeyDict, CaseDict
 from .texts import (
     str_find_any, str_split_ex, str_sanitize, str_scan_sub,
@@ -12,6 +13,7 @@ from .quant import Quantity
 
 __all__ = [
     'base64url_encode', 'base64url_decode',
+    'list_find_ex',
     'TransKeyDict', 'CaseDict',
     'str_find_any', 'str_split_ex', 'str_sanitize', 'str_scan_sub',
     'str_encode_nonprints', 'str_decode_nonprints',
